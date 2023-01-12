4ac4a55b4827fcf559aa29b4512719b5ccccf3c8
wuxh <wxh1220@gmail.com>
2023-01-12 16:46:22 +0800

    update

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,8 @@
 // more config: https://d.umijs.org/config
 import { defineConfig } from 'dumi';
 
-const basePath = process.env.GH_PAGES ? '/test-ci/' : '/';
-const publicPath = process.env.GH_PAGES ? '/test-ci/' : '/';
+const basePath = process.env.GITHUB_ACTIONS ? '/test-ci/' : '/';
+const publicPath = process.env.GITHUB_ACTIONS ? '/test-ci/' : '/';
 
 export default defineConfig({
   base: basePath,
