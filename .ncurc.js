00990bd2ac304b1fbdb3e88f4a69e5c91500136e
wuxh <wxh1220@gmail.com>
2023-10-30 14:22:04 +0800

    chore: copy antd package
    ci: add upgrade-deps
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update
    
    chore: update rule
    
    chore: update
    
    chore: update
    
    chore: update

---
+++ .ncurc.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+// https://github.com/react-component/
+const rcOrg = ['@rc-component/', 'rc-'];
+const check = ['@ant-design/', ...rcOrg];
+
+module.exports = {
+  packageFile: path.resolve(__dirname, './package.json'),
+  upgrade: false, // use `npx npm-check-updates -u` to upgrade
+  target: 'semver', // package.json semver
+  packageManager: 'npm',
+  dep: ['prod'], // check only prod dependencies
+  // https://github.com/raineorshine/npm-check-updates#filter
+  filter: (name) => check.some((prefix) => name.startsWith(prefix)),
+};
