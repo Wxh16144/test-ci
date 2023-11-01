01e99424da219965c6fa2b53f7de8ccd9dfa4490
wuxh <wxh1220@gmail.com>
2023-11-01 14:29:36 +0800

    chore: update

--- .ncurc.js
+++ .ncurc.js
@@ -7,9 +7,22 @@ const check = ['@ant-design/', ...rcOrg];
 module.exports = {
   packageFile: path.resolve(__dirname, './package.json'),
   upgrade: false, // use `npx npm-check-updates -u` to upgrade
-  target: 'semver', // package.json semver
   packageManager: 'npm',
   dep: ['prod'], // check only prod dependencies
   // https://github.com/raineorshine/npm-check-updates#filter
   filter: (name) => check.some((prefix) => name.startsWith(prefix)),
+  // https://github.com/raineorshine/npm-check-updates#target
+  target: (name, semver) => {
+    const { operator } = semver[0] ?? {};
+
+    // rc-component
+    if (rcOrg.some((prefix) => name.startsWith(prefix))) {
+      // `^` always upgrade latest, otherwise follow semver.
+      if (operator === '^') {
+        return 'latest';
+      }
+    }
+
+    return 'semver';
+  }
 };
